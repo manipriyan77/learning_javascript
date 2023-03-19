a3be179e776397b1e3c89372a4bef76b188f2248
manipriyan77 <manipriyangopalan@gmail.com>
2023-03-19 20:39:33 +0530

    finished fundamentals 1

---
+++ script.js
@@ -0,0 +1,12 @@
+// 1.Values and variables
+let name = "Manipriyan";
+let age = 24;
+let isMajor = true;
+
+console.log(name);
+
+// 2.let const var
+let myAge = 24;
+myAge = 25;
+const myName = "Manipriyan";
+myName = "Mani";
